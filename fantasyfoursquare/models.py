c71cb4b3c04dfd5560ceed0acf8311fdeb8cf18b
Nicole Lee <nkl9092@gmail.com>
2011-10-02 07:29:56 -0400

    added models

---
+++ fantasyfoursquare/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class User(models.Model):
+	username = models.CharField(max_length=200)
+	points = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.username
+
+class Place(models.Model):
+	name = models.CharField(max_length=200)
+	type = models.CharField(max_length=200)
+	latitude = models.CharField(max_length=200)
+	longitude = models.CharField(max_length=200)
+	original_hits = models.IntegerField()
+	current_hits = models.IntegerField()
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.name
